c77e9dc3a83b6049daa070d560ddd863c84499b6
ranauzair <ranauzair044@gmail.com>
2024-04-28 23:07:03 +0500

    adding admin password

--- src/scripts/seedDB.js
+++ src/scripts/seedDB.js
@@ -3,9 +3,9 @@ const { User } = require("../models/index");
 const bcrypt = require("bcrypt");
 const createAdmin = async () => {
   const admin = [
-  {
-    email: "ranauzair044@gmail.com",
-    password: "qwerty@1",
+    {
+      email: "ranauzair044@gmail.com",
+      password: bcrypt.hashSync("Password@1", 8),
     roleId: 1,
     createdAt: new Date(),
     updatedAt: new Date(),
